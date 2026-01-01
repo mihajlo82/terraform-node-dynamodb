4e59538fc77056be17b9880e2773a53de39473fd
Mihajlo <dimitricmihajlo995@gmail.com>
2026-01-01 14:45:13 +0100

    final cc

--- app.js
+++ app.js
@@ -96,4 +96,9 @@ app.delete('/users/:id', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+// app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
